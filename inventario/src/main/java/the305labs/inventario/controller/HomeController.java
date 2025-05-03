83626691f378f2937c860d0f5d4abdd2f86d1176
Rav <freze000@gmail.com>
2025-05-03 16:42:02 -0500

    Update HomeController.java

--- inventario/src/main/java/the305labs/inventario/controller/HomeController.java
+++ inventario/src/main/java/the305labs/inventario/controller/HomeController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    // mvn spring-boot:run
+    //mysql -u root -p
 
     @GetMapping("/")
     public String home() {
