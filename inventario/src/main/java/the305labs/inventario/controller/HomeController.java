1f7d7a6ad37a48a9136ab0edb3b8ce21de27653d
Rav <freze000@gmail.com>
2025-05-03 15:29:54 -0500

    fron page

---
+++ inventario/src/main/java/the305labs/inventario/controller/HomeController.java
@@ -0,0 +1,13 @@
+package the305labs.inventario.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "prueba 123";
+    }
+}
--- inventario/src/main/java/the305labs/inventario/repository/MovimientoInventarioRepository.java
+++ inventario/src/main/java/the305labs/inventario/repository/MovimientoInventarioRepository.java
@@ -3,5 +3,8 @@ package the305labs.inventario.repository;
 import the305labs.inventario.entity.MovimientoInventario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface MovimientoInventarioRepository extends JpaRepository<MovimientoInventario, Long> {
+
+    
 }
